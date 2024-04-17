ad17c056fcb154acb0625322a87e7b8e7fb926e3
PedroBWDF <bwdf64587@gmail.com>
2024-04-17 12:17:58 +0800

    support facebook OAuth2.0 login/register

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,56 @@ const router = express.Router()
 
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const FacebookStrategy = require('passport-facebook')
 const bcrypt = require('bcryptjs')
 
 const db = require('../models')
 const User = db.User
+
+passport.use(new FacebookStrategy({
+  clientID: process.env.FACEBOOK_CLIENT_ID,
+  clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+  //取得Facebook的email跟顯示名稱
+  profileFields: ['email', 'displayName']
+}, (accessToken, refreshToken, profile, done) => {
+  //用戶可能不只一個email，取主要的
+  const email = profile.emails[0].value
+  const name = profile.displayName
+
+  return User.findOne({
+    attributes: ['id', 'name', 'email'],
+    where: { email },
+    raw: true
+  })
+    .then((user) => {
+      //若user用臉書登錄過，即回傳給done函式
+      if (user) return done(null, user)
+
+      //若無，隨機創建一組密碼
+      const randomPwd = Math.random().toString(36).slice(-8)
+
+      return bcrypt.hash(randomPwd, 10)
+        //密碼加密後儲存在DB
+        .then((hash) => User.create({ name, email, password: hash }))
+        //返回給done函式
+        .then((user) => done(null, { id: user.id, name: user.name, email: user.email }))
+    })
+    .catch((error) => {
+      error.errorMessage = '登入失敗'
+      done(error)
+    })
+  //下方教案再進行實作
+
+}))
+
+router.get('/login/facebook', passport.authenticate('facebook', { scope: ['email'] }))
+
+router.get('/oauth2/redirect/facebook', passport.authenticate('facebook', {
+  successRedirect: '/todos',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
 
 passport.use(new LocalStrategy({ usernameField: 'email' }, (username, password, done) => {
   return User.findOne({
